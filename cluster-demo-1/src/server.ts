5da7f23308813a00681d85175c9817655a727257
Kartik Lakhani <kartik.lakhani@bacancy.com>
2025-05-02 11:54:28 +0530

    feat: Implement clustering in Express server for improved performance
    - Added support for clustering using the cluster module
    - Forks worker processes based on available CPU threads
    - Each worker serves requests independently, enhancing scalability
    - Updated response message to include worker process ID

--- cluster-demo-1/src/server.ts
+++ cluster-demo-1/src/server.ts
@@ -1,12 +1,32 @@
 import express from 'express';
+import cluster from 'cluster';
+import os from 'os';
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+import process from 'process';
 
-app.get('/', (_, res) => {
-  res.send('Hello from Express + TypeScript!');
-});
+const numCPUs = os.cpus().length;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+const totalThreads = os.availableParallelism();
+
+
+
+if(cluster.isPrimary){
+  console.log(`Master ${process.pid} is running`);
+  console.log(`Total threads: ${totalThreads}` , " total cpus : ", numCPUs);
+  for(let i = 0; i < totalThreads; i++){
+    cluster.fork();
+  }
+}else{
+  
+  const app = express();
+  const PORT = process.env.PORT || 3000;
+
+  app.get('/', (_, res) => {
+    res.send('Hello from Express + TypeScript! from worker ' + process.pid);
+  });
+
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT} from worker ${process.pid}`);
+  });
+  
+}
